d9e432eeebf1ce63c9252f341d6d3f14bc32e796
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-24 17:32:19 +0330

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Kết nối đến MongoDB
+mongoose.connect('mongodb://localhost:27017/food_data', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Kết nối tới MongoDB thành công!'))
+  .catch(err => console.error(err));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với API phân tích dữ liệu thực phẩm!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
+});
