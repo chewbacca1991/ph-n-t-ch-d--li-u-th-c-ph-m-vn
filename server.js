e0c3e3a92ee78fe5a13a75fdeb4f53f0ef86ec37
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-24 17:35:47 +0330

    Update MongoDB connection message for clarity

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/food_data';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Successfully connected to MongoDB!'))
-  .catch(err => console.error(err));
+  .then(() => console.log('Connected to MongoDB successfully!'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(bodyParser.json());
 
