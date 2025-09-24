09a3ac6bc52af9f954c06d34a7186a6d406b687a
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-24 17:32:22 +0330

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Phân Tích Dữ Liệu Thực Phẩm Việt Nam</h1>
+      <p>Chào mừng bạn đến với ứng dụng!</p>
+    </div>
+  );
+}
+
+export default App;
